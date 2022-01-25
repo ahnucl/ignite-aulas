3e95f6f960f595e96d45c11d1d5d4d3c9eb14f84
Leonardo <lalmcunha@gmail.com>
2022-01-25 14:10:27 -0300

    Cria banco de dados fake

--- reactjs/dtmoney/src/components/TransactionsTable/index.tsx
+++ reactjs/dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,10 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  type: 'deposit' | 'withdraw';
+  category: string;
+  amount: number;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
   useEffect(() => {
-    api.get('transactions').then(response => console.log(response.data));
+    api.get('transactions').then(response => setTransactions(response.data.transactions));
   },[])
     
   return (
@@ -19,22 +30,15 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-              <td>Desenvolvimento de website</td>
-              <td className="deposit">R$12.000</td>
-              <td>Desenvolvimento</td>
-              <td>20/01/2022</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}> 
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
-        <tbody>
-          <tr>
-              <td>Aluguel</td>
-              <td className="withdraw">- R$1.000</td>
-              <td>Casa</td>
-              <td>17/01/2022</td>
-          </tr>
-        </tbody>
-
       </table>
     </Container>
     )
