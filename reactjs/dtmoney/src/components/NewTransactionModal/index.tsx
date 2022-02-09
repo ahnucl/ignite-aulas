747f1fca457d19ec56ce643a6350ec8b00c8a68e
Leonardo <lalmcunha@gmail.com>
2022-02-09 11:39:45 -0300

    Finaliza módulo de transações

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { api } from '../../services/api'
+import { useTransactions } from '../../hooks/useTransactions'
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 interface NewTransactionModalProps {
@@ -15,22 +15,23 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose
 }: NewTransactionModalProps){
-  const [title, setTitle] = useState('')
-  const [value, setValue] = useState(0)
-  const [category, setCategory] = useState('')
-  const [type, setType] = useState('deposit')
+  const { createTransaction } = useTransactions()
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  const [title, setTitle] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [category, setCategory] = useState('')
+  const [type, setType] = useState<'deposit' | 'withdraw'>('deposit')
+
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
     
-    const data = {
-      title,
-      value,
-      category,
-      type,
-    }
+    await createTransaction({ title, amount, category, type })
 
-    api.post('transactions', data)
+    setTitle('')
+    setAmount(0)
+    setCategory('')
+    setType('deposit')
+    onRequestClose()
   }
 
   return(
@@ -60,8 +61,8 @@ export function NewTransactionModal({
         <input 
           type="number" 
           placeholder="Valor" 
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
