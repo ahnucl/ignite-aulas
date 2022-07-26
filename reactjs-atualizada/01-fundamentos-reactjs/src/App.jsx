42a22203d8430c99fd3af3e7075241c39c0e77e8
Leonardo <lalmcunha@gmail.com>
2022-07-26 08:58:39 -0300

    Início rota atualizada projeto 1

---
+++ reactjs-atualizada/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./post"
+
+export function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+    </>
+  )
+}
