42386888b5a04d316f45ce5fa45edd355dda7e15
Leonardo <lalmcunha@gmail.com>
2022-07-26 18:09:53 -0300

    Completa App

--- reactjs-atualizada/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-atualizada/01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,31 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post } from "./post"
+
+import styles from './App.module.css'
+
+import './global.css'
 
 export function App() {
   return (
-    <>
-      <h1>Hello</h1>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-    </>
+    <div>
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Leonardo"
+            content="as dajdlasj dashdj landa"
+          />
+          <Post 
+            author="Leonardo"
+            content="as dajdlasj dashdj landa"
+          />
+        </main>
+      </div>
+
+      
+    </div>
   )
 }
