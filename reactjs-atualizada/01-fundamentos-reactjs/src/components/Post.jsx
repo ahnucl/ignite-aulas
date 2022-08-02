7977fb9d99380357e3d7d6bb282c02333505efb1
Leonardo Cunha <lalmcunha@gmail.com>
2022-08-02 10:15:22 -0300

    Atualiza com cõdigo perdido

--- reactjs-atualizada/01-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs-atualizada/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,31 +1,76 @@
-import styles from './Post.module.css'
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
-export function Post() {
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+
+import styles from './Post.module.css';
+
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
+  function handleCrateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1]);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img src="https://github.com/ahnucl.png"/>
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Leonardo Cunha</strong>
-            <span>Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        
-        <time dateTime="2022-07-27 16:00:00" title="27 de julho às 16:00h">Publicado a 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Oasndkasnd kasud as</p>
-        <p>asdhbasjdasjhdgsajhdgjashd  asd asd asd asd asd asd </p>
-        <p>👉{' '}<a href="">lalmcunha@gmail.com</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>!
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
-    )
+  )
 }
--- reactjs-atualizada/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ reactjs-atualizada/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,26 +1,29 @@
-import { PencilSimpleLine } from "phosphor-react";
-import styles from "./Sidebar.module.css";
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      {/* Unsplash, alrgura max 500px, qualidade 50% pra menos */}
-      <img 
+      <img
         className={styles.cover}
-        src="https://images.unsplash.com/photo-1658763440414-07d3b9c42bb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
+
       <div className={styles.profile}>
-        <img src="https://github.com/ahnucl.png" className={styles.avatar}/>
-        <strong>Leonardo Cunha</strong>
-        <span>Developer</span>
+        <Avatar src="https://github.com/maykbrito.png" />
+
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="">
-          <PencilSimpleLine size={20}/>
+        <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
     </aside>
-  )
+  );
 }
