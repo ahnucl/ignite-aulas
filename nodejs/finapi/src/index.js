fa9e25b74c9208d70af9fa3bf3e6f5e84a58e131
Leonardo Cunha <lalmcunha@gmail.com>
2022-08-15 20:32:51 -0300

    Implementa várias regras

--- nodejs/finapi/src/index.js
+++ nodejs/finapi/src/index.js
@@ -6,6 +6,32 @@ app.use(express.json())
 
 const customers = []
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" })
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
+function getBalance(statement) {
+  return statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount
+    } else if (operation.type === 'debit') {
+      return acc - operation.amount
+    }
+  }, 0)
+}
+
+
 /**
  * cpf - string
  * name - string
@@ -31,17 +57,62 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
-app.get('/statement/', (request, response) => {
-  const { cpf } = request.headers
+app.use(verifyIfExistsAccountCPF)
 
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" })
-  }
+app.get('/statement', (request, response) => {
+  const { customer } = request
 
   return response.json(customer.statement)
 })
 
+app.post('/deposit', (request, response) => {
+  const { customer } = request
+  const { description, amount } = request.body // sem validação do amount
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+app.post('/withdraw', (request, response) => {
+  const { amount } = request.body // sem validação
+  const { customer } = request
+
+  const balance = getBalance(customer.statement)  
+  
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds"})
+  }
+
+  const statementOperation = {
+    description: "Saque",
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+app.get('/statement/date', (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+
+  const dateFormat = new Date(date + " 00:00") // hack
+
+  const statementByDate = customer.statement.filter(statement =>
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return response.json(statementByDate)
+})
 
 app.listen(3333)
