0add61130c034650fe265a9c14176791daa1edb4
Leonardo Cunha <lalmcunha@gmail.com>
2022-08-23 20:48:39 -0300

    Adiciona contexto para sidebar drawer

--- reactjs/dashgo/src/components/Header/Profile.tsx
+++ reactjs/dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProficeData: boolean
+}
+
+export function Profile({ showProficeData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Leonardo Cunha</Text>
-        <Text color="gray.300" fontSize="small">
-          lalmcunha@gmail.com
-        </Text>
-      </Box>
+      {showProficeData && (
+        <Box mr="4" textAlign="right">
+          <Text>Leonardo Cunha</Text>
+          <Text color="gray.300" fontSize="small">
+            lalmcunha@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
