f6375d823f37781862f2ff40f643986b0c60d600
Leonardo Cunha <lalmcunha@gmail.com>
2022-08-26 23:40:52 -0300

    Automatiza formulário com react-hook-form

--- reactjs/dashgo/src/pages/index.tsx
+++ reactjs/dashgo/src/pages/index.tsx
@@ -1,11 +1,29 @@
-import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
 
 /**
  * alignItems e align são iguais, assim como w e width, justifyContent e justyfy, etc
  */
 
+type SignInFormData = {
+  email: string
+  password: string
+}
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm()
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async values => {
+    await new Promise(resolve => {
+      setTimeout(resolve, 2000)
+    })
+
+    console.log(values)
+  }
+
+  // const { errors } = formState
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -16,14 +34,31 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input label="E-mail" name="email" type="email" />
+          <Input
+            label="E-mail"
+            name="email"
+            type="email"
+            {...register('email')}
+          />
 
-          <Input label="Senha" name="password" type="password" />
+          <Input
+            label="Senha"
+            name="password"
+            type="password"
+            {...register('password')}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
