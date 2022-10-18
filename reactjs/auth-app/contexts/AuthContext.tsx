f5e7ce701cd6b08afae25e22ef5e31d74489147b
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-10-17 21:53:42 -0300

    Cria verificação de refresh token no contexto

--- reactjs/auth-app/contexts/AuthContext.tsx
+++ reactjs/auth-app/contexts/AuthContext.tsx
@@ -1,4 +1,13 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from "nookies";
+import Router from 'next/router'
+import { api } from "../services/api";
+
+interface User {
+  email: string
+  permissions: string[]
+  roles: string[]
+}
 
 interface SignInCredentials {
   email: string
@@ -7,6 +16,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>
+  user?: User
   isAuthenticated: boolean
 }
 
@@ -17,14 +27,58 @@ interface AuthProviderProps {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false
+  const [user, setUser] = useState<User>()
+  const isAuthenticated = !!user
   
+  useEffect(() => {
+    const { 'auth-app.token': token } = parseCookies()
+
+    if(token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({ email, permissions, roles })
+      })
+    }
+
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
-    console.log({email, password})
+    try {
+
+      const response = await api.post('sessions', {
+        email, 
+        password,
+      })
+      
+      const { token, refreshToken, permissions, roles } = response.data
+
+      setCookie(undefined, 'auth-app.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/', // quais caminhos da aplicação terão acesso ao cookie
+      })
+      
+      setCookie(undefined, 'auth-app.refreshToken', refreshToken,{
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/', // quais caminhos da aplicação terão acesso ao cookie
+      })
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+      Router.push('/dashboard')
+    } catch (err) {
+      console.error(err)
+    }
   }
   
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
---
+++ reactjs/auth-app/services/api.ts
@@ -0,0 +1,78 @@
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
+
+let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue: any[] = []
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3333',
+  headers: {
+    Authorization: `Bearer ${cookies['auth-app.token']}`
+  }
+})
+
+
+interface ApiError {
+   error: boolean,
+   code: string,
+   message: string
+}
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: any) => { // AxiosError<ApiError>
+  if (error.response?.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies()
+
+      const { 'auth-app.refreshToken': refreshToken} = cookies
+      const originalConfig = error.config
+
+      if(!isRefreshing) {
+        isRefreshing = true
+
+        api.post('/refresh', {
+          refreshToken,
+        }).then(response => {
+          const { token } = response.data
+  
+          setCookie(undefined, 'auth-app.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/', // quais caminhos da aplicação terão acesso ao cookie
+          })
+  
+          setCookie(undefined, 'auth-app.refreshToken', response.data.refreshToken,{
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/', // quais caminhos da aplicação terão acesso ao cookie
+          })
+
+          api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+          failedRequestsQueue.forEach(request => request.onSuccess(token))
+          failedRequestsQueue = []
+        }).catch(err => {
+          failedRequestsQueue.forEach(request => request.onFailure(err))
+          failedRequestsQueue = []
+        }).finally(() => {
+          isRefreshing = false
+        })
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestsQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+            resolve(api(originalConfig))
+          },
+          onFailure: (err: AxiosError) => {
+            reject(err)
+          },
+        })
+      })
+    } else {
+      // deslogar o usuário
+    }
+  }
+})
