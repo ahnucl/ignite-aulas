be1a1dfd756c0e05de5fe044320f4124fec1fb36
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-10-21 21:54:14 -0300

    Finaliza com log out de usuário em erros que não o token expirado e adiciona tratativas para demais erros nas chamadas da API

--- reactjs/auth-app/contexts/AuthContext.tsx
+++ reactjs/auth-app/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from "nookies";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 import Router from 'next/router'
 import { api } from "../services/api";
 
@@ -16,8 +16,16 @@ interface SignInCredentials {
 
 interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>
-  user?: User
+  // user: User
+  user?: User // linha acima é a da aula
   isAuthenticated: boolean
+}
+
+export function signOut() {
+  destroyCookie(undefined, 'auth-app.token')
+  destroyCookie(undefined, 'auth-app.refreshToken')
+
+  Router.push('/')
 }
 
 export const AuthContext = createContext({} as AuthContextData)
@@ -29,24 +37,26 @@ interface AuthProviderProps {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
-  
+
   useEffect(() => {
     const { 'auth-app.token': token } = parseCookies()
 
     if(token) {
-      api.get('/me').then(response => {
-        console.log('response no AuthContext', response)
-        const { email, permissions, roles } = response.data
+      api.get('/me')
+        .then(response => {
+          const { email, permissions, roles } = response.data
 
-        setUser({ email, permissions, roles })
-      })
+          setUser({ email, permissions, roles })
+        })
+        .catch(() => {
+          signOut()
+        })
     }
 
   }, [])
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
-
       const response = await api.post('sessions', {
         email, 
         password,
@@ -74,7 +84,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
       Router.push('/dashboard')
     } catch (err) {
-      console.error(err)
+      console.log(err)
     }
   }
   
--- reactjs/auth-app/services/api.ts
+++ reactjs/auth-app/services/api.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosError } from 'axios'
+import axios, { AxiosError, AxiosRequestConfig } from 'axios'
 import { parseCookies, setCookie } from 'nookies'
+import { signOut } from '../contexts/AuthContext'
 
 let cookies = parseCookies()
 let isRefreshing = false
@@ -21,30 +22,30 @@ interface ApiError {
 
 api.interceptors.response.use(response => {
   return response
-}, (error: AxiosError) => { // AxiosError<ApiError>
-  if (error?.response?.status === 401) {
-    if (error.response.data?.code === 'token.expired') {
+}, (error: AxiosError<ApiError>) => { // AxiosError<ApiError>
+  if (error.response?.status === 401) {
+    if (error.response.data.code === 'token.expired') {
       cookies = parseCookies()
 
-      const { 'auth-app.refreshToken': refreshToken} = cookies
-      const originalConfig = error.config
+      const { 'auth-app.refreshToken': refreshToken } = cookies
+      const originalConfig = error.config as AxiosRequestConfig
 
-      if(!isRefreshing) {
-        isRefreshing = true
+      if (!isRefreshing) {
+        isRefreshing = true // faz request de /refresh apenas uma vez
 
         api.post('/refresh', {
           refreshToken,
         }).then(response => {
           const { token } = response.data
-  
+
           setCookie(undefined, 'auth-app.token', token, {
             maxAge: 60 * 60 * 24 * 30, // 30 days
-            path: '/', // quais caminhos da aplicação terão acesso ao cookie
+            path: '/',
           })
-  
+
           setCookie(undefined, 'auth-app.refreshToken', response.data.refreshToken,{
             maxAge: 60 * 60 * 24 * 30, // 30 days
-            path: '/', // quais caminhos da aplicação terão acesso ao cookie
+            path: '/',
           })
 
           api.defaults.headers['Authorization'] = `Bearer ${token}`
@@ -60,22 +61,25 @@ api.interceptors.response.use(response => {
       }
 
       return new Promise((resolve, reject) => {
-        console.log('dentro da fila')
         failedRequestsQueue.push({
           onSuccess: (token: string) => {
-            console.log('Success')
-            originalConfig.headers['Authorization'] = `Bearer ${token}`
-            
+            originalConfig.headers!.Authorization = `Bearer ${token}` // precisei adicionar o ! para tirar erro do TS
+            console.log('Success', originalConfig)  
+            const data = api(originalConfig)
             resolve(api(originalConfig))
           },
           onFailure: (err: AxiosError) => {
-            console.log('Failure')
             reject(err)
           },
         })
       })
     } else {
-      // deslogar o usuário
+      // deslogar o usuário, pois foi algum erro de autenticação mas não por expiração do token
+      signOut()
     }
   }
+  
+  // Aparentemente não retornar algo no interceptor dá problema de undefined nas outras requisições
+  // return Promise.resolve(error.response) // só passa a response com o erro 401 para o cliente
+  return Promise.reject(error)
 })
