a9cb3068e4ff01f50e540cb5a70e8d20d19ef2ba
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2022-10-25 17:32:42 -0300

    Cria hook inicial para permissões/roles

--- reactjs/auth-app/pages/dashboard.tsx
+++ reactjs/auth-app/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react"
-import { AuthContext } from "../contexts/AuthContext"
+import { AuthContext, signOut } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -7,13 +8,33 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = useCan({
+    // permissions: ['metrics.list']
+    roles: ['administrator']
+  })
+
   useEffect(() => {
     api.get('/me')
       .then(response => console.log('dashboard', response))
       .catch((error) => console.log('dashboard-error', error))
   }, [])
 
-  return <h1>Dashboard: { user?.email }</h1>
+  return (
+    <>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '0 10px'
+      }}>
+        <h1>Dashboard: { user?.email }</h1>
+        <div>
+          <button onClick={signOut}>Sair</button>
+        </div>
+      </div>
+      { userCanSeeMetrics && <div>Métricas</div> }
+    </>
+  )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
