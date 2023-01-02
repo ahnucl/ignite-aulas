d8cdc8fcb078736f8eded92294adf022bade9459
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2023-01-02 11:15:34 -0300

    Cria variável de resumo

--- reactjs-atualizada/03-dtmoney/src/components/Summary/index.tsx
+++ reactjs-atualizada/03-dtmoney/src/components/Summary/index.tsx
@@ -10,6 +10,27 @@ export function Summary() {
   
   const { transactions } = useContext(TransactionsContext)
 
+  // { income: 0, outcome: 0, total: 0 }
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    }, 
+    { 
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -19,7 +40,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={theme["green-300"]}/>
         </header>
 
-        <strong>R$ 17.400</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -29,7 +50,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={theme["red-300"]}/>
         </header>
 
-        <strong>R$ 17.400</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
@@ -38,7 +59,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={theme.white}/>
         </header>
 
-        <strong>R$ 17.400</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
