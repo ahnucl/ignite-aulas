fc60be35d64742de86098723ba9783b212dc8675
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-01-03 11:19:35 -0300

    Inicia projeto 04 com NextJS

---
+++ reactjs-atualizada/04-ignite-shop/src/pages/404.tsx
@@ -0,0 +1,17 @@
+export default function FourOhFour() {
+  return (
+    <div
+      style={{
+        // width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <span>:(</span>
+      <span>Binbin</span>
+    </div>
+  )
+}
