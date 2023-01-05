89cf4f44027c3544bc989c8635be01aae73bd6b7
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-01-05 14:55:41 -0300

    Ajusta next para usar imagens do stripe

--- reactjs-atualizada/04-ignite-shop/next.config.js
+++ reactjs-atualizada/04-ignite-shop/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // domains: ['files.stripe.com'], // Testar
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+        pathname: '/links/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
