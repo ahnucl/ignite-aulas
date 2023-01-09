a2c9239b2de85b7ba4c3bbd852be126434dc4b0c
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2023-01-09 16:38:29 -0300

    Pega produtos via SSG

--- reactjs-atualizada/04-ignite-shop/src/pages/product/[id].tsx
+++ reactjs-atualizada/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,31 +1,84 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from '../../styles/pages/product'
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <p>Loading</p>
+  }
 
   return (
     <ProductContainer>
-      <ImageContainer>{/* <Image></Image> */}</ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
-          corrupti, magni incidunt quo iure sequi magnam assumenda repellat
-          ipsum tempora. Expedita et sit nihil doloribus repellendus laborum
-          animi sunt fugit!
-        </p>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  // Buscar os produtos mais vendidos / mais acessados
+
+  return {
+    paths: [{ params: { id: 'prod_N6UB7ZqJeVvWxz' } }],
+    fallback: true,
+  }
+}
+
+// Primeiro parâmetro do generics -> tipage das props retornada
+// Segundo parâmetro do generics -> tipagem do params
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params!.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format((price.unit_amount as number) / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  }
 }
