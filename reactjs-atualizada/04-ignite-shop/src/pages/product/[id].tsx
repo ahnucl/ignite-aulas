2dcb2f0c61e53f69f2aae179f2f1e73a8cb3cc26
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2023-01-09 12:15:19 -0300

    Cria estrutura e estilo da página de produto

--- reactjs-atualizada/04-ignite-shop/src/pages/product/[id].tsx
+++ reactjs-atualizada/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,31 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image></Image> */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+          corrupti, magni incidunt quo iure sequi magnam assumenda repellat
+          ipsum tempora. Expedita et sit nihil doloribus repellendus laborum
+          animi sunt fugit!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
