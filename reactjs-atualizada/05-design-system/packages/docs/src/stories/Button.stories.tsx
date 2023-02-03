813ff23ea3379605c34d45bf4c776a5cdc1ca285
Leonardo Cunha <lalmcunha@gmail.com>
2023-02-03 18:11:37 -0300

    Começa configuração do storybook (usar node 16)

---
+++ reactjs-atualizada/05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,10 @@
+import { StoryObj, Meta } from '@storybook/react'
+
+import { Button } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  compoonent: Button,
+} as Meta
+
+export const Primary: StoryObj = {}
