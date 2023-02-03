9863433472e9abd1fec3cea54f1bd895b5f28b95
Leonardo Cunha <lalmcunha@gmail.com>
2023-02-03 16:52:09 -0300

    Adiciona storybook

--- reactjs-atualizada/05-design-system/packages/react/src/index.tsx
+++ reactjs-atualizada/05-design-system/packages/react/src/index.tsx
@@ -3,6 +3,8 @@ import { styled } from './styles'
 const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
 })
 
 export function App() {
