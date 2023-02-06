5f810a4a05e31bab09c0d746488a256b9bc0ffd2
Leonardo Cunha <lalmcunha@gmail.com>
2023-02-06 17:30:03 -0300

    Configura storybook corretamente

--- reactjs-atualizada/05-design-system/packages/docs/src/stories/Button.stories.tsx
+++ reactjs-atualizada/05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -1,10 +1,20 @@
-import { StoryObj, Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 
-import { Button } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
   title: 'Button',
-  compoonent: Button,
-} as Meta
+  component: Button,
 
-export const Primary: StoryObj = {}
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
