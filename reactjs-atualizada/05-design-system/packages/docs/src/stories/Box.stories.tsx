772ff81d5b1587ac20cb8bc81a6c27ca68c1efba
Leonardo Cunha <lalmcunha@gmail.com>
2023-02-09 11:23:35 -0300

    Cria componente Text

--- reactjs-atualizada/05-design-system/packages/docs/src/stories/Box.stories.tsx
+++ reactjs-atualizada/05-design-system/packages/docs/src/stories/Box.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { Box, BoxProps } from '@ignite-ui/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
 
 export default {
   title: 'Surfaces/Box',
@@ -8,7 +8,7 @@ export default {
   args: {
     children: (
       <>
-        <span>Testanto o elemento Box</span>
+        <Text>Testanto o elemento Box</Text>
       </>
     ),
   },
---
+++ reactjs-atualizada/05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et optio vel, tempore maiores error alias ducimus modi sint, dolorem eveniet dolorum nulla magnam neque corporis unde? Sequi nam quo debitis.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
