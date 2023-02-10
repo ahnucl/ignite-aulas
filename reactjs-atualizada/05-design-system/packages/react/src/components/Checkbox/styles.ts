448f40e29e4af583cea1bb149af5a3dc6963c6f9
Leonardo Cunha <lalmcunha@gmail.com>
2023-02-10 11:05:50 -0300

    Finaliza estilização do Checkbox

--- reactjs-atualizada/05-design-system/packages/react/src/components/Checkbox/styles.ts
+++ reactjs-atualizada/05-design-system/packages/react/src/components/Checkbox/styles.ts
@@ -1,5 +1,5 @@
 import * as Checkbox from '@radix-ui/react-checkbox'
-import { styled } from '../../styles'
+import { styled, keyframes } from '../../styles'
 
 export const CheckboxContainer = styled(Checkbox.Root, {
   all: 'unset',
@@ -16,8 +16,31 @@ export const CheckboxContainer = styled(Checkbox.Root, {
   alignItems: 'center',
   border: '2px solid $gray900',
 
+  '&[data-state="checked"]': {
+    backgroundColor: '$ignite300',
+    border: '2px solid $ignite300',
+  },
+
   '&:focus': {
     border: '2px solid $ignite300',
+  },
+})
+
+const slideIn = keyframes({
+  from: {
+    transform: 'translateY(-100%)',
+  },
+  to: {
+    transform: 'translateY(0)',
+  },
+})
+
+const slideOut = keyframes({
+  from: {
+    transform: 'translateY(0)',
+  },
+  to: {
+    transform: 'translateY(-100%)',
   },
 })
 
@@ -25,4 +48,12 @@ export const CheckboxIndicator = styled(Checkbox.Indicator, {
   color: '$white',
   width: '$4',
   height: '$4',
+
+  '&[data-state="checked"]': {
+    animation: `${slideIn} 200ms ease-out`,
+  },
+
+  '&[data-state="unchecked"]': {
+    animation: `${slideOut} 200ms ease-out`,
+  },
 })
