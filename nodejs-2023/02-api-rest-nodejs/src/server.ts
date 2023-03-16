0f997660612fee73e2f3ce6421ddad1b280ea4a2
Leonardo Cunha <lalmcunha@gmail.com>
2023-03-16 17:43:48 -0300

    Testa BD

--- nodejs-2023/02-api-rest-nodejs/src/server.ts
+++ nodejs-2023/02-api-rest-nodejs/src/server.ts
@@ -1,16 +1,23 @@
-import { knex } from './database'
 import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
 
 const app = fastify()
 
 // GET, POST, PUT, PATCH, DELETE
 
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return test
+  const transactions = await knex('transactions').select('*')
 
-  return 'Backend online 🚀'
+  return transactions
 })
 
 app
