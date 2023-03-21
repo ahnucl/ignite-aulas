498b44b28d55472b5a7dade41fca0306404922fd
Leonardo Cunha <lalmcunha@gmail.com>
2023-03-21 11:56:05 -0300

    Cria plugin de transactions

--- nodejs-2023/02-api-rest-nodejs/src/routes/transactions.ts
+++ nodejs-2023/02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,12 +1,29 @@
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 0)
-      .select('*')
+  app.post('/', async (request, reply) => {
+    // { title, amount, type: 'credito' | 'debito'}
 
-    return transactions
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    // Rotas de API - não fazer retornos
+    return reply.status(201).send()
   })
 }
