135fbdba607e7d3764fbd7f3e38f99e5d5ade03b
Leonardo Cunha <lalmcunha@gmail.com>
2023-03-28 11:49:51 -0300

    Ajusta contexto de testes

--- nodejs-2023/02-api-rest-nodejs/test/example.spec.ts
+++ nodejs-2023/02-api-rest-nodejs/test/example.spec.ts
@@ -1,5 +1,25 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, test, describe, it } from 'vitest'
+import { app } from '../src/app'
 
-test('', () => {
-  expect()
+describe('Transactions route', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  // test('user can create a new transaction', async () => {
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
