ed69227764471c1bcf38c5599da3007dbb2b9ca7
Leonardo Cunha <lalmcunha@gmail.com>
2023-03-29 15:56:32 -0300

    Ajustes pra deploy

--- nodejs-2023/02-api-rest-nodejs/src/database.ts
+++ nodejs-2023/02-api-rest-nodejs/src/database.ts
@@ -2,10 +2,13 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL, // relativo ao local de execução do código
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL, // relativo ao local de execução do código
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
