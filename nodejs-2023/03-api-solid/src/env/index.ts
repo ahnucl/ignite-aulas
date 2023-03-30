1292fe920edb9ffe9370dcda5d29d6e3ce1437e7
Leonardo Cunha <lalmcunha@gmail.com>
2023-03-30 15:40:56 -0300

    Instala eslint

--- nodejs-2023/03-api-solid/src/env/index.ts
+++ nodejs-2023/03-api-solid/src/env/index.ts
@@ -1,10 +1,12 @@
 // import { config } from "dotenv";
 import 'dotenv/config'
-import { z } from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-  PORT: z.coerce.number().default(3333)
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
