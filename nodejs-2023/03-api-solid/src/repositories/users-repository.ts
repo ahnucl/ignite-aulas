9277d1e5b19b8db72bb7b2ea16687aec66339e31
Leonardo Cunha <lalmcunha@gmail.com>
2023-04-06 09:39:22 -0300

    Adiciona comentário de dúvida

--- nodejs-2023/03-api-solid/src/repositories/users-repository.ts
+++ nodejs-2023/03-api-solid/src/repositories/users-repository.ts
@@ -1,5 +1,6 @@
 import { Prisma, User } from '@prisma/client'
 
+// O fato da minha interface depender da tipagem do prisma não seria um problema?
 export interface UsersRepository {
   findByEmail(email: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
