fc4935ae093866eab39893503f31fe40d8170857
Leonardo Cunha <lalmcunha@gmail.com>
2023-04-06 15:09:27 -0300

    Cria método de busca por id no repositório de usuários

--- nodejs-2023/03-api-solid/src/repositories/users-repository.ts
+++ nodejs-2023/03-api-solid/src/repositories/users-repository.ts
@@ -2,6 +2,7 @@ import { Prisma, User } from '@prisma/client'
 
 // O fato da minha interface depender da tipagem do prisma não seria um problema?
 export interface UsersRepository {
+  findById(id: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
 }
