5724670af4b9eba6d875bc7e605a7a65004a1668
Leonardo Cunha <lalmcunha@gmail.com>
2023-04-06 12:07:40 -0300

    Cria caso de uso de autenticação e testes

--- nodejs-2023/03-api-solid/src/use-cases/register.spec.ts
+++ nodejs-2023/03-api-solid/src/use-cases/register.spec.ts
@@ -5,11 +5,11 @@ import { InMemoryUsersRepository } from './../repositories/in-memory/in-memory-u
 import { RegisterUseCase } from './register'
 
 describe('Register Use Case', () => {
-  it('should be able to', async () => {
+  it('should be able to register', async () => {
     const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const sut = new RegisterUseCase(usersRepository)
 
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
