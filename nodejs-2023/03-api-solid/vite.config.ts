95463312a2684f46b023bdc1d903c41e4d8dab3c
Leonardo Cunha <lalmcunha@gmail.com>
2023-04-11 12:07:56 -0300

    Cria ambiente de testes para o vite

--- nodejs-2023/03-api-solid/vite.config.ts
+++ nodejs-2023/03-api-solid/vite.config.ts
@@ -3,4 +3,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      ['src/http/controllers/**', 'prisma'], // segunda posição precisa ser exatamente o nome que vem após o nome da pasta "vitest-environment-"
+    ],
+  },
 })
