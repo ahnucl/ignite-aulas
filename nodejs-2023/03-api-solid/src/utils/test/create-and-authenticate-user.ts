02f4f5033fa9606a7f832c519826a1fd1806313a
Leonardo Cunha <lalmcunha@gmail.com>
2023-04-12 14:41:49 -0300

    Separa criação de usuário e autenticação em função

---
+++ nodejs-2023/03-api-solid/src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+interface Response {
+  token: string
+}
+
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+): Promise<Response> {
+  await request(app.server).post('/users').send({
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password: '123456',
+  })
+
+  const authResponse = await request(app.server).post('/sessions').send({
+    email: 'johndoe@example.com',
+    password: '123456',
+  })
+
+  const { token } = authResponse.body
+
+  return {
+    token,
+  }
+}
