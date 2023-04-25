2e657ef75dfa4c559dae6735f4a45535e1777a47
Leonardo Cunha <lalmcunha@gmail.com>
2023-04-25 11:05:16 -0300

    Cria rota de horários

---
+++ reactjs-atualizada/06-ignite-call/src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,62 @@
+import { prisma } from '@/lib/prisma'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username) // não existe params no Next - o query retorna tanto parâmetros de rotas quanto de query (com ?)
+  const { date } = req.query // http://localhost:3333/api/users/leocunha/availability?date=2022-03-19
+
+  if (!date) {
+    // Erro que dificilmente ocorrerá para um usuário sem conhecimento de tecnologia
+    return res.status(400).json({ message: 'Date not provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exists.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('date').isBefore(new Date()) // API não pode depender de validações feitas no front
+
+  if (isPastDate) {
+    return res.json({ availability: [] })
+  }
+
+  // Cruzamento de dados entre Time Interval e Schedulings
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.json({ availability: [] })
+  }
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60 // end time is not avaiable
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => {
+      return startHour + i
+    },
+  )
+
+  return res.json({ possibleTimes })
+}
