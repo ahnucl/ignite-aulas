fa21a8f414217fccd0e78470cb405ef1bd8fbf9a
Leonardo Cunha <lalmcunha@gmail.com>
2023-06-01 10:07:20 -0300

    Cria getters e setters

--- nodejs-2023/04-clean-ddd/src/domain/entities/answer.ts
+++ nodejs-2023/04-clean-ddd/src/domain/entities/answer.ts
@@ -12,8 +12,39 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> { 
+  get authorId() {
+    return this.props.authorId
+  }
+  
+  get questionId() {
+    return this.props.questionId
+  }
+  
   get content() {
     return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  private touch() { // método privado! Deve ser usado em outros métodos que mudam informações
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
   }
 
   static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
