a382c2c7eb7bdd5578de97e51239b8eaa6f1842e
Leonardo Cunha <lalmcunha@gmail.com>
2023-06-07 09:40:05 -0300

    Cria caso de uso para edição de perguntas

---
+++ nodejs-2023/04-clean-ddd/src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,57 @@
+import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { EditQuestionUseCase } from './edit-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository) // system under test
+  })
+
+  it('should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toString(),
+      authorId: 'author-1',
+      title: 'Novo título',
+      content: 'Novo conteúdo',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'Novo título',
+      content: 'Novo conteúdo',
+    })
+  })
+
+  it('should not be able to edit a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(() => {
+      return sut.execute({
+        questionId: newQuestion.id.toString(),
+        authorId: 'author-2',
+        title: 'Novo título',
+        content: 'Novo conteúdo',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
