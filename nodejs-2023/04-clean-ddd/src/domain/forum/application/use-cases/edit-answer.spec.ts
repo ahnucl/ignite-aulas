8c196d179b0d6bbcb2bb53fbbb08cb0c50f7267f
Leonardo Cunha <lalmcunha@gmail.com>
2023-06-08 12:05:31 -0300

    Finaliza casos de uso do CRUD

---
+++ nodejs-2023/04-clean-ddd/src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,55 @@
+import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { EditAnswerUseCase } from './edit-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: EditAnswerUseCase
+
+describe('Edit answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository) // system under test
+  })
+
+  it('should be able to edit a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      answerId: newAnswer.id.toString(),
+      authorId: 'author-1',
+      content: 'Novo conteúdo',
+    })
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      title: 'Novo título',
+      content: 'Novo conteúdo',
+    })
+  })
+
+  it('should not be able to edit a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    expect(() => {
+      return sut.execute({
+        answerId: newAnswer.id.toString(),
+        authorId: 'author-2',
+        content: 'Novo conteúdo',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
