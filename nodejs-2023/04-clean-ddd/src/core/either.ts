e2fa20fa52291ef1a9a39ed4827d8f813d96ceb5
Leonardo Cunha <lalmcunha@gmail.com>
2023-06-19 09:43:10 -0300

    Finaliza testes do either

--- nodejs-2023/04-clean-ddd/src/core/either.ts
+++ nodejs-2023/04-clean-ddd/src/core/either.ts
@@ -1,22 +1,38 @@
 // Error
-export class Left<L> {
+export class Left<L, R> {
   readonly reason: L
 
   constructor(reason: L) {
     this.reason = reason
   }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
 }
 
 // Success
-export class Right<R> {
+export class Right<L, R> {
   readonly reason: R
 
   constructor(reason: R) {
     this.reason = reason
   }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
 }
 
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, R> = Left<L, R> | Right<L, R>
 
 export const left = <L, R>(reason: L): Either<L, R> => {
   return new Left(reason)
