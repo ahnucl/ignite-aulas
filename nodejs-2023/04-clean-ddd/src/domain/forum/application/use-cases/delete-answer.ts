0f0bfa1aa175b994c483d523ff5bd0131fdf5cf7
Leonardo Cunha <lalmcunha@gmail.com>
2023-06-20 11:08:39 -0300

    Aplica tratamento de erros funcional a todos os casos de uso

--- nodejs-2023/04-clean-ddd/src/domain/forum/application/use-cases/delete-answer.ts
+++ nodejs-2023/04-clean-ddd/src/domain/forum/application/use-cases/delete-answer.ts
@@ -1,3 +1,6 @@
+import { NotAllowedError } from './errors/not-allowed-error'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { Either, left, right } from '@/core/either'
 import { AnswersRepository } from '../repositories/answers-repository'
 
 interface DeleteAnswerUseCaseRequest {
@@ -5,7 +8,10 @@ interface DeleteAnswerUseCaseRequest {
   answerId: string
 }
 
-interface DeleteAnswerUseCaseResponse {}
+type DeleteAnswerUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>
 
 export class DeleteAnswerUseCase {
   constructor(private answersRepository: AnswersRepository) {}
@@ -17,15 +23,15 @@ export class DeleteAnswerUseCase {
     const answer = await this.answersRepository.findById(answerId)
 
     if (!answer) {
-      throw new Error('Answer not found.')
+      return left(new ResourceNotFoundError())
     }
 
     if (authorId !== answer.authorId.toString()) {
-      throw new Error('Not allowed.')
+      return left(new NotAllowedError())
     }
 
     await this.answersRepository.delete(answer)
 
-    return {}
+    return right({})
   }
 }
