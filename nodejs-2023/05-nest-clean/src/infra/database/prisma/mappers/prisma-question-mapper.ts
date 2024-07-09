687b5e7cbcd4470f912fd9ee4c96da7cd2c9939c
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-09 14:52:38 -0300

    Implementa primeiro método do repositório com mapper

---
+++ nodejs-2023/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,25 @@
+/**
+ * Mappers são classes responsáveis por converter uma entidade de um formato de uma camada para formato de outra camada
+ */
+
+import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Question as PrismaQuestion } from '@prisma/client'
+
+export class PrismaQuestionMapper {
+  static toDomain(raw: PrismaQuestion): Question {
+    return Question.create(
+      {
+        title: raw.title,
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        bestAnswerId: undefined,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
