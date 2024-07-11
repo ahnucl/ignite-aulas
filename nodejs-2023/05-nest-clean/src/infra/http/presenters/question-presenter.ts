9b212726a588f722572e5ea11f3ae37261b6d4a6
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-11 14:07:17 -0300

    Cria presenter

---
+++ nodejs-2023/05-nest-clean/src/infra/http/presenters/question-presenter.ts
@@ -0,0 +1,19 @@
+import { Question } from '@/domain/forum/enterprise/entities/question'
+
+export class QuestionPresenter {
+  static toHTTP(question: Question) {
+    return {
+      id: question.id.toString(),
+      title: question.title,
+      slug: question.slug.value,
+      bestAnswerId: question.bestAnswerId?.toString(),
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
+  }
+}
+
+/**
+ * Em REST deve-se retornar apenas os campos necessários, caso seja preciso em
+ * outro momento dados em outros formatos, deve-se criar um novo presenter
+ */
