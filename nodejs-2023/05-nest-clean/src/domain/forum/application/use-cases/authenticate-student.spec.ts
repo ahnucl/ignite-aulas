cf08daeedefa337f5911c01ff3f8fd46f623d7d8
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-12 12:36:22 -0300

    Cria testes para registro e autenticação de alunos

---
+++ nodejs-2023/05-nest-clean/src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -0,0 +1,61 @@
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { CreateQuestionUseCase } from './create-question'
+import { RegisterStudentUseCase } from './register-student'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { AuthenticateStudentUseCase } from './authenticate-student'
+import { makeStudent } from 'test/factories/make-student'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+
+let sut: AuthenticateStudentUseCase
+
+describe('Authenticate Student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+
+    sut = new AuthenticateStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate a student', async () => {
+    const student = makeStudent({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.create(student)
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.reason).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+
+  it('should hash student password upon registration', async () => {
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0]).toEqual(hashedPassword)
+  })
+})
