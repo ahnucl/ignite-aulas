0e892cbb2a43ae5558278d9e4e58c535a97487ba
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-22 15:07:29 -0300

    Atualiza casos de uso

---
+++ nodejs-2023/05-nest-clean/src/infra/database/prisma/mappers/prisma-student-mapper.ts
@@ -0,0 +1,29 @@
+/**
+ * Mappers são classes responsáveis por converter uma entidade de um formato de uma camada para formato de outra camada
+ */
+
+import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
+import { Student } from '@/domain/forum/enterprise/entities/student'
+import { Prisma, User as PrismaUser } from '@prisma/client'
+
+export class PrismaStudentMapper {
+  static toDomain(raw: PrismaUser): Student {
+    return Student.create(
+      {
+        name: raw.name,
+        email: raw.email,
+        password: raw.password,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+
+  static toPrisma(student: Student): Prisma.UserUncheckedCreateInput {
+    return {
+      id: student.id.toString(),
+      name: student.name,
+      email: student.email,
+      password: student.password,
+    }
+  }
+}
