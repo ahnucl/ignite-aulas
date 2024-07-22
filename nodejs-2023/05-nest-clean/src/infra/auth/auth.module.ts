f54acd61e7bb94ae3734b1c9f9d4d77874db4677
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-22 16:34:05 -0300

    Melhora consumo de variáveis ambiente

--- nodejs-2023/05-nest-clean/src/infra/auth/auth.module.ts
+++ nodejs-2023/05-nest-clean/src/infra/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import { Env } from '@/infra/env'
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
+import { EnvService } from '../env/env.service'
 import { JwtAuthGuard } from './jwt-auth.guard'
 import { JwtStrategy } from './jwt.strategy'
+import { EnvModule } from '../env/env.module'
 
 /**
  * Algoritmo RS256 - chave privada e chave publica
@@ -17,12 +17,13 @@ import { JwtStrategy } from './jwt.strategy'
   imports: [
     PassportModule,
     JwtModule.registerAsync({
+      imports: [EnvModule],
       // Quando for necessário usar alguma injeção de dependência - Tem na doc
-      inject: [ConfigService],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+      useFactory(env: EnvService) {
+        const privateKey = env.get('JWT_PRIVATE_KEY')
+        const publicKey = env.get('JWT_PUBLIC_KEY')
 
         return {
           signOptions: { algorithm: 'RS256' },
@@ -34,6 +35,7 @@ import { JwtStrategy } from './jwt.strategy'
   ],
   providers: [
     JwtStrategy,
+    EnvService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
