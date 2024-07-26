e1d2ef9dfb9781c04650ea60e5edb68290c58032
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-26 10:47:35 -0300

    Transforma repositóries em classes abstratas

--- nodejs-2023/05-nest-clean/src/domain/forum/application/repositories/answers-repository.ts
+++ nodejs-2023/05-nest-clean/src/domain/forum/application/repositories/answers-repository.ts
@@ -1,13 +1,14 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Answer } from '../../enterprise/entities/answer'
 
-export interface AnswersRepository {
-  findById(id: string): Promise<Answer | null>
-  findManyByQuestionId(
+export abstract class AnswersRepository {
+  abstract findById(id: string): Promise<Answer | null>
+  abstract findManyByQuestionId(
     questionId: string, // Não precisa passar dentro do objeto pois o próprio método indica que esse é o primeiro parâmetro
     params: PaginationParams, // o objeto se refere a paginação apenas! No momento apenas temos o número da página, mas poderíamos ter queries de busca por exemplos
   ): Promise<Answer[]>
-  create(answer: Answer): Promise<void>
-  save(answer: Answer): Promise<void>
-  delete(answer: Answer): Promise<void>
+
+  abstract create(answer: Answer): Promise<void>
+  abstract save(answer: Answer): Promise<void>
+  abstract delete(answer: Answer): Promise<void>
 }
