15ed182aff8b95a7000302905343de54de60c0ce
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-29 10:55:35 -0300

    Renomeia campo para refletir autor genérico e não apenas instrutor

--- nodejs-2023/05-nest-clean/src/domain/forum/application/use-cases/answer-question.ts
+++ nodejs-2023/05-nest-clean/src/domain/forum/application/use-cases/answer-question.ts
@@ -4,9 +4,10 @@ import { Answer } from '../../enterprise/entities/answer'
 import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { Injectable } from '@nestjs/common'
 
 interface AnswerQuestionUseCaseRequest {
-  instructorId: string
+  authorId: string
   questionId: string
   attachmentsIds: string[]
   content: string
@@ -23,14 +24,14 @@ export class AnswerQuestionUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
   async execute({
-    instructorId,
+    authorId,
     questionId,
     content,
     attachmentsIds,
   }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
-      authorId: new UniqueEntityID(instructorId),
+      authorId: new UniqueEntityID(authorId),
       questionId: new UniqueEntityID(questionId),
     })
 
