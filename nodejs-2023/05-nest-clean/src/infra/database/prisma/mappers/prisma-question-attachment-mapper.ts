ca8160d4117eb07be64776e96ef6f602d1e8dfaa
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-08-19 11:31:34 -0300

    Envia anexos no teste de criar pergunta

--- nodejs-2023/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ nodejs-2023/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -1,10 +1,12 @@
 /**
  * Mappers são classes responsáveis por converter uma entidade de um formato de uma camada para formato de outra camada
+ *
+ * Mappers estão sempre intimamente ligados com os repostórios
  */
 
 import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
-import { Attachment as PrismaAttachment } from '@prisma/client'
+import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
 
 export class PrismaQuestionAttachmentMapper {
   static toDomain(raw: PrismaAttachment): QuestionAttachment {
@@ -19,5 +21,24 @@ export class PrismaQuestionAttachmentMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: QuestionAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) =>
+      attachment.attachmentId.toString(),
+    )
+
+    return {
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+      data: {
+        questionId: attachments[0].questionId.toString(),
+      },
+    }
   }
 }
