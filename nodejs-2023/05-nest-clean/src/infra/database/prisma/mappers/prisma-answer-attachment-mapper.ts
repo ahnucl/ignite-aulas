bde4c20858f7ffcc740012aa6d141add3155cd35
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-08-21 10:02:14 -0300

    Corrige envio de anexos nas respostas na camada http

--- nodejs-2023/05-nest-clean/src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
+++ nodejs-2023/05-nest-clean/src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -4,7 +4,7 @@
 
 import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
 import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
-import { Attachment as PrismaAttachment } from '@prisma/client'
+import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
 
 export class PrismaAnswerAttachmentMapper {
   static toDomain(raw: PrismaAttachment): AnswerAttachment {
@@ -19,5 +19,24 @@ export class PrismaAnswerAttachmentMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: AnswerAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) =>
+      attachment.attachmentId.toString(),
+    )
+
+    return {
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+      data: {
+        answerId: attachments[0].answerId.toString(),
+      },
+    }
   }
 }
