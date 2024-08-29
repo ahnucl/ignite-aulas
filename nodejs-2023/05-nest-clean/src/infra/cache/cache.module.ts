569540fbfccb09db5b60274f62830aea640ca30e
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-08-29 15:27:11 -0300

    Implementa Cache com Redis

--- nodejs-2023/05-nest-clean/src/infra/cache/cache.module.ts
+++ nodejs-2023/05-nest-clean/src/infra/cache/cache.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common'
+import { RedisService } from '../database/redis/redis.service'
 import { EnvModule } from '../env/env.module'
+import { CacheRepository } from './cache-repository'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
 import { EnvService } from '../env/env.service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    EnvService,
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
